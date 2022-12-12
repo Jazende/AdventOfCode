d8b6c136860a9a0390146c4aec01221b7a9eeb8d
Jazende <smirkloph@gmail.com>
2022-12-12 15:47:26 +0100

    day 6

---
+++ aoc22/22_06.py
@@ -0,0 +1,21 @@
+with open(r'22_06.txt', 'r') as f:
+    raw_lines = f.read().strip()
+
+idx = 0
+four = raw_lines[idx:idx+4]
+while True:
+    if len(set(four)) == 4:
+        break
+    four = four[1:] + raw_lines[idx+4]
+    idx += 1
+
+print(idx+4)
+
+fourteen = raw_lines[idx:idx+14]
+while True:
+    if len(set(fourteen)) == 14:
+        break
+    fourteen = fourteen[1:] + raw_lines[idx+14]
+    idx += 1
+
+print(idx+14)
