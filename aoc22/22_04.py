da455a69340964d809a20ac24a7b49ab38da0c66
Jazende <smirkloph@gmail.com>
2022-12-12 13:07:59 +0100

    day 4 & 5

---
+++ aoc22/22_04.py
@@ -0,0 +1,25 @@
+with open(r'22_04.txt', 'r') as f:
+    raw_lines = f.read().strip()
+
+assignments = [(*line.split(',')[0].split('-'), *line.split(',')[1].split('-')) for line in raw_lines.split('\n')]
+
+def intify(assignment):
+    assignment = [int(x) for x in assignment]
+    return assignment
+
+def encompasses(assignment):
+    if (assignment[0] <= assignment[2] and assignment[1] >= assignment[3]) or (assignment[2] <= assignment[0] and assignment[3] >= assignment[1]):
+        return True
+    return False
+
+print(sum(1 if encompasses(intify(assignment)) else 0 for assignment in assignments))
+
+def overlaps(assignment):
+    if (assignment[2] <= assignment[0] <= assignment[3]) or \
+        (assignment[2] <= assignment[1] <= assignment[3]) or \
+        (assignment[0] <= assignment[2] <= assignment[1]) or \
+        (assignment[0] <= assignment[3] <= assignment[1]):
+        return True
+    return False
+
+print(sum(1 if overlaps(intify(assignment)) else 0 for assignment in assignments))
---
+++ aoc22/22_05.py
@@ -0,0 +1,54 @@
+import re
+
+with open(r'22_05.txt', 'r') as f:
+    raw_lines = f.read().strip()
+
+re_move_order = re.compile('move (\d+) from (\d+) to (\d+)')
+orders = re_move_order.findall(raw_lines)
+
+def intify(order):
+    return [int(x) for x in order]
+
+crates = [
+    ['M', 'J', 'C', 'B', 'F', 'R', 'L', 'H'], 
+    ['Z', 'C', 'D', ], 
+    ['H', 'J', 'F', 'C', 'N', 'G', 'W', ], 
+    ['P', 'J', 'D', 'M', 'T', 'S', 'B', ], 
+    ['N', 'C', 'D', 'R', 'J', ], 
+    ['W', 'L', 'D', 'Q', 'P', 'J', 'G', 'Z', ],
+    ['P', 'Z', 'T', 'F', 'R', 'H', ],
+    ['L', 'V', 'M', 'G', ], 
+    ['C', 'B', 'G', 'P', 'F', 'Q', 'R', 'J', ], 
+]
+
+def execute_order_9000(amount, from_, to_):
+    for _ in range(amount):
+        crates[to_-1].append(crates[from_-1].pop(len(crates[from_-1])-1))
+
+for order in orders:
+    execute_order_9000(*intify(order))
+
+print(''.join(crate[-1] for crate in crates))
+
+## Day 2
+
+crates = [
+    ['M', 'J', 'C', 'B', 'F', 'R', 'L', 'H'], 
+    ['Z', 'C', 'D', ], 
+    ['H', 'J', 'F', 'C', 'N', 'G', 'W', ], 
+    ['P', 'J', 'D', 'M', 'T', 'S', 'B', ], 
+    ['N', 'C', 'D', 'R', 'J', ], 
+    ['W', 'L', 'D', 'Q', 'P', 'J', 'G', 'Z', ],
+    ['P', 'Z', 'T', 'F', 'R', 'H', ],
+    ['L', 'V', 'M', 'G', ], 
+    ['C', 'B', 'G', 'P', 'F', 'Q', 'R', 'J', ], 
+]
+
+def execute_order_9001(amount, from_, to_):
+    crates[to_-1] += crates[from_-1][-amount:]
+    crates[from_-1] = crates[from_-1][0:len(crates[from_-1])-amount]
+
+for order in orders:
+    execute_order_9001(*intify(order))
+
+print(''.join(crate[-1] for crate in crates))
