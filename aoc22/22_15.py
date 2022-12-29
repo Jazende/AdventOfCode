bf44f9533173060bd35aa168415ff18dfd205fc3
Jazende <smirkloph@gmail.com>
2022-12-29 11:01:38 +0100

    dramatically improved part 2

--- aoc22/22_15.py
+++ aoc22/22_15.py
@@ -1,7 +1,7 @@
+import cProfile
 import re
 from operator import attrgetter
-import cProfile
-from functools import partial
+from itertools import product, combinations, chain
 
 with open(r'22_15.txt', 'r') as f:
     raw_lines = f.read().strip()
@@ -13,24 +13,6 @@ min_y = 0
 max_y = 4_000_000
 
 re_sensors = re.compile('Sensor at x=([0-9\-]+), y=([0-9\-]+): closest beacon is at x=([0-9\-]+), y=([0-9\-]+)')
-
-# raw_lines = '''Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-# Sensor at x=9, y=16: closest beacon is at x=10, y=16
-# Sensor at x=13, y=2: closest beacon is at x=15, y=3
-# Sensor at x=12, y=14: closest beacon is at x=10, y=16
-# Sensor at x=10, y=20: closest beacon is at x=10, y=16
-# Sensor at x=14, y=17: closest beacon is at x=10, y=16
-# Sensor at x=8, y=7: closest beacon is at x=2, y=10
-# Sensor at x=2, y=0: closest beacon is at x=2, y=10
-# Sensor at x=0, y=11: closest beacon is at x=2, y=10
-# Sensor at x=20, y=14: closest beacon is at x=25, y=17
-# Sensor at x=17, y=20: closest beacon is at x=21, y=22
-# Sensor at x=16, y=7: closest beacon is at x=15, y=3
-# Sensor at x=14, y=3: closest beacon is at x=15, y=3
-# Sensor at x=20, y=1: closest beacon is at x=15, y=3'''
-# row_to_check = y = 10
-# max_x = 20
-# max_y = 20
 
 def manhattan_distance(first_x, first_y, second_x, second_y):
     return abs(first_x - second_x) + abs(first_y - second_y)
@@ -48,15 +30,35 @@ class Sensor:
         horizontal_distance = self.range - vertical_distance
         return range(self.x - horizontal_distance, self.x + horizontal_distance+1)
 
-    def bounds_on_height(self, height):
-        vertical_distance   = abs(self.y - height)
-        horizontal_distance = self.range - vertical_distance
-        return (self.x - horizontal_distance), (self.x + horizontal_distance)
+    # def inner_lines(self):
+    #     print('-x {', self.x - self.range, '<= x <=', self.x, '} -', self.range, '+', self.x, '+', self.y)
+    #     print('-x {', self.x, '<= x <=', self.x + self.range, '} +', self.range, '+', self.x, '+', self.y)
+    #     print(' x {', self.x, '<= x <=', self.x + self.range, '} -', self.range, '-', self.x, '+', self.y)
+    #     print(' x {', self.x - self.range, '<= x <=', self.x, '} +', self.range, '-', self.x, '+', self.y)
+
+    # def outer_lines(self):
+    #     print('-x {', self.x - (self.range + 1), '<= x <=', self.x, '} -', (self.range + 1), '+', self.x, '+', self.y)
+    #     print('-x {', self.x, '<= x <=', self.x + (self.range + 1), '} +', (self.range + 1), '+', self.x, '+', self.y)
+    #     print(' x {', self.x, '<= x <=', self.x + (self.range + 1), '} -', (self.range + 1), '-', self.x, '+', self.y)
+    #     print(' x {', self.x - (self.range + 1), '<= x <=', self.x, '} +', (self.range + 1), '-', self.x, '+', self.y)
+
+    def line_segments_up(self):
+        return (
+            {'offset': self.y - self.x - (self.range + 1), 'min': self.x, 'max': self.x + (self.range + 1)},
+            {'offset': self.y - self.x + (self.range + 1), 'min': self.x - (self.range + 1), 'max': self.x},
+        )
+
+    def line_segments_down(self):
+        return (
+            {'offset': self.y + self.x - (self.range + 1), 'min': self.x - (self.range + 1), 'max': self.x},
+            {'offset': self.y + self.x + (self.range + 1), 'min': self.x, 'max': self.x + (self.range + 1)}
+        )
 
     def __repr__(self):
         return f'S@({self.x}, {self.y}):{self.range}'
 
 sensors = [Sensor(*x) for x in re_sensors.findall(raw_lines)]
+sensors.sort(key=attrgetter('x'))
 beacons = set((sensor.beacon_x, sensor.beacon_y) for sensor in sensors)
 
 def calculate_invalid_locations(sensors, beacons, y):
@@ -69,58 +71,45 @@ def calculate_invalid_locations(sensors, beacons, y):
     return invalid_locations
 
 invalid_locations = calculate_invalid_locations(sensors, beacons, y)
-print('Part 1: ', len(invalid_locations))
+print('Part 1:', len(invalid_locations))
 
-def find_valid_range(invalid_ranges):
-    return invalid_ranges[0][1]+1
+##########################################################
 
-def combine_ranges(ranges):
-    idx = 0
-    while True:
-        if len(ranges) < 2 or idx + 1 >= len(ranges):
-            break
-        if ranges[idx][0] <= ranges[idx+1][0] <= ranges[idx][1] and ranges[idx][0] <= ranges[idx+1][1] <= ranges[0][1]:
-            ranges.pop(idx+1)
-            idx = 0
-        elif ranges[idx+1][0] <= ranges[idx][1] + 1:
-            ranges[idx][1] = ranges[idx+1][1]
-            ranges.pop(idx+1)
-            idx = 0
-        else:
-            idx += 1
-    return ranges
+def find_missing_beacon(sensors):
+    points = set()
+    for sensor_combination in combinations(sensors, 2):
+        first_sensor, second_sensor = sensor_combination
+        # Get a chain of first_up, second_down & second_up, first_down; this to always have first segment go up and second go down for calculations
+        # Could be replaced by a double loop for clarity's sake
+        for line_combo in chain(product(first_sensor.line_segments_up(), second_sensor.line_segments_down()), product(second_sensor.line_segments_up(), first_sensor.line_segments_down())):
+            # offset_a = up, offset_b = down
+            offset_a, offset_b = line_combo
+            # x + a = -x + b -> x + x = b - a
+            # 2x = b - a     -> x = (b - a) / 2
 
-def _bounded_range(range_, lower_bound, upper_bound):
-    low, high = range_
-    low = max(lower_bound, low)
-    high = min(upper_bound, high)
-    return (low, high)
+            double_point_of_contact = (offset_b['offset'] - offset_a['offset'])
+            if not double_point_of_contact % 2 == 0:
+                continue
+            point_of_contact = double_point_of_contact / 2
+            if not offset_a['min'] <= point_of_contact <= offset_a['max']:
+                continue
+            if not offset_b['min'] <= point_of_contact <= offset_b['max']:
+                continue
 
-bounded_range = partial(_bounded_range, lower_bound=min_x, upper_bound=max_x)
+            # x + slope_a = y                  || - x + slope_b = y
+            # point_of_contact + slope_a = y   || - point_of_contact + slope_b = y
+            height = point_of_contact + offset_a['offset']
 
-def invalid_locations_at_height(sensors, beacons, y):
-    raw_ranges = []
-    for sensor in sensors:
-        left_bound, right_bound = sensor.bounds_on_height(y)
-        if left_bound >= right_bound:
-            continue
-        raw_ranges.append([left_bound, right_bound])
-    raw_ranges.sort()
+            # check that both x and y are between 0 and max
+            if not 0 <= point_of_contact <= max_x:
+                continue
+            if not 0 <= height <= max_y:
+                continue
+            points.add((int(point_of_contact), int(height)))
 
-    combined_ranges = combine_ranges(raw_ranges)
-    bounded_ranges = list(map(bounded_range, combined_ranges))
-    return bounded_ranges
+    for point in points:
+        point_x, point_y = point
+        if not any(manhattan_distance(point_x, point_y, sensor.x, sensor.y) <= sensor.range for sensor in sensors):
+            return point_x * 4_000_000 + point_y
 
-def find_valid_solutions(sensors, beacons, min_y, max_y):
-    invalid_range_check = [(min_x, max_x)]
-    for height in range(min_y, max_y):
-        invalid_ranges = invalid_locations_at_height(sensors, beacons, height)
-        if invalid_ranges == invalid_range_check:
-            continue
-        valid_range = find_valid_range(invalid_ranges)
-        break
-    return valid_range * 4_000_000 + height
-
-cProfile.run('find_valid_solutions(sensors, beacons, min_y, max_y)')
-solutions = find_valid_solutions(sensors, beacons, min_y, max_y)
-print('Part 2: ', solutions)
+print('Part 2:', find_missing_beacon(sensors))
